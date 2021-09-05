0b08acfc2e56efd5b06fafb17de0be80739dac55
keistuol1z <skanusmakaronas6@gmail.com>
2021-09-05 11:18:42 +0300

    namu uzduoties 3 klausimas

---
+++ namu_darbas_3klausimas/namu_darbas_3klausimas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace namu_darbas_3klausimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+              Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
